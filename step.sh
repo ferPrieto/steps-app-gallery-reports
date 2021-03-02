93b2205093cfbce89a6d5991a5c79770e41373e3
Fernando Prieto Moyano <f.prieto.moyano@gmail.com>
2021-03-02 17:45:54 +0000

    Added rest of calls and needed parameters

--- step.sh
+++ step.sh
@@ -39,8 +39,81 @@ function getDownloadAndInstallationReport()
   printf "\nGetting Download&Installation Report - DONE\n"
 }
 
+function getInAppPaymentReport()
+{
+  ACCESS_TOKEN=`jq -r '.access_token' token.json` 
+
+  printf "\nGetting In-App Payment Report\n"
+
+  curl --silent -X GET \
+  'https://connect-api.cloud.huawei.com/api/report/distribution-operation-quality/v1/IAPExport/'"${huawei_app_id}"'?language='"${in_app_payment_language}"'&startTime='"${in_app_payment_start_time}"'&endTime='"${in_app_payment_end_time}"'&exportType='"${in_app_payment_export_type}"'&currency='"${in_app_payment_currency}" \
+  -H 'Authorization: Bearer '"${ACCESS_TOKEN}"'' \
+  -H 'client_id: '"${huawei_client_id}"'' > DownloadAndInstallationReport.json
+  REPORT_LINK=`jq -r '.fileURL' InAppPaymentReport.json` 
+  envman add --key "IN_APP_PAYMENT_REPORT_REPORT_LINK" --value "$REPORT_LINK"
+  printf "Log URL obtained (\${IN_APP_PAYMENT_REPORT_REPORT_LINK}=${REPORT_LINK})"
+
+  printf "\nGetting In-App Payment Report - DONE\n"
+}
+
+function getPaidDownloadReport()
+{
+  ACCESS_TOKEN=`jq -r '.access_token' token.json` 
+  # Get a token depending on the domain selected
+  DOMAIN=$( getDomainFromSelection ) 
+
+  printf "\nGetting Paid Download Report\n"
+
+  curl --silent -X GET \
+  'https://"${DOMAIN}"/api/report/distribution-operation-quality/v1/orderDetailExport/'"${huawei_app_id}"'?language='"${paid_download_language}"'&startTime='"${paid_download_start_time}"'&endTime='"${paid_download_end_time}"'&exportType='"${paid_download_export_type}"'&filterCondition='"countryId"'&filterConditionValue='"${paid_download_country_id}" \
+  -H 'Authorization: Bearer '"${ACCESS_TOKEN}"'' \
+  -H 'client_id: '"${huawei_client_id}"'' > PaidDownloadReport.json
+  REPORT_LINK=`jq -r '.fileURL' PaidDownloadReport.json` 
+  envman add --key "PAID_DOWNLOAD_REPORT_LINK" --value "$REPORT_LINK"
+  printf "Log URL obtained (\${PAID_DOWNLOAD_REPORT_LINK}=${REPORT_LINK})"
+
+  printf "\nGetting Paid Download Report - DONE\n"
+}
+
+function getInstallationFailureDataReport()
+{
+  ACCESS_TOKEN=`jq -r '.access_token' token.json` 
+
+  printf "\nGetting Installation Failure Data Report\n"
+
+  curl --silent -X GET \
+  'https://connect-api.cloud.huawei.com/api/report/distribution-operation-quality/v1/appDownloadFailExport/'"${huawei_app_id}"'?language='"${installation_failure_data_language}"'&startTime='"${installation_failure_data_start_time}"'&endTime='"${installation_failure_data_end_time}" \
+  -H 'Authorization: Bearer '"${ACCESS_TOKEN}"'' \
+  -H 'client_id: '"${huawei_client_id}"'' > PaidDownloadReport.json
+  REPORT_LINK=`jq -r '.fileURL' InstallationFailureDataReport.json` 
+  envman add --key "INSTALLATION_FAILURE_DATA_REPORT_LINK" --value "$REPORT_LINK"
+  printf "Log URL obtained (\${INSTALLATION_FAILURE_DATA_REPORT_LINK}=${REPORT_LINK})"
+
+  printf "\nGetting Installation Failure Data Report - DONE\n"
+}
+
+function getDomainFromSelection()
+{ 
+    if [ "${paid_download_domain}" == "China" ] ;then
+        DOMAIN="connect-api.cloud.huawei.com"
+    elif [ "${paid_download_domain}" == "Europe" ] ;then 
+        DOMAIN="connect-api-dre.cloud.huawei.com"
+    elif [ "${paid_download_domain}" == "Asia Pacific" ] ;then 
+        DOMAIN="connect-api-dra.cloud.huawei.com"
+    else  
+        DOMAIN="connect-api-drru.cloud.huawei.com"
+    fi 
+    echo $DOMAIN
+}
+
 getToken  
 
 getDownloadAndInstallationReport 
+
+getInAppPaymentReport
+
+getPaidDownloadReport
+
+getInstallationFailureDataReport
 
 exit 0
