cc1b39c5c21298cff8a995173b573c77b906464b
Fernando Prieto Moyano <f.prieto.moyano@gmail.com>
2021-03-03 15:45:29 +0000

    In app payment report fixed

--- step.sh
+++ step.sh
@@ -8,10 +8,12 @@ fi
 
 function getToken()
 {
+  TOKEN_URL=$( getTokenUrl ) 
+
   printf "\n\nObtaining a Token\n"  
   
   curl --silent -X POST \
-    https://connect-api.cloud.huawei.com/api/oauth2/v1/token \
+    "${TOKEN_URL}" \
     -H 'Content-Type: application/json' \
     -H 'cache-control: no-cache' \
     -d '{
@@ -23,11 +25,26 @@ function getToken()
   printf "\nObtaining a Token - DONE\n"
 } 
 
+function getTokenUrl()
+{
+   if [ "${paid_download_domain}" == "Russia" ] ;then
+        URL="https://connect-api-drru.cloud.huawei.com/api/oauth2/v1/token"
+    elif [ "${paid_download_domain}" == "Europe" ] ;then 
+        URL="https://connect-api-dre.cloud.huawei.com/api/oauth2/v1/token"
+    elif [ "${paid_download_domain}" == "Asia Pacific" ] ;then 
+        URL="https://connect-api-dra.cloud.huawei.com/api/oauth2/v1/token"
+    else  
+        URL="https://connect-api.cloud.huawei.com/api/oauth2/v1/token"
+    fi 
+    echo $URL
+} 
+
 function getDownloadAndInstallationReport()
 {
   ACCESS_TOKEN=`jq -r '.access_token' token.json` 
 
   printf "\nGetting Download&Installation Report\n"
+  
   curl --silent -X GET \
   'https://connect-api.cloud.huawei.com/api/report/distribution-operation-quality/v1/appDownloadExport/'"${huawei_app_id}"'?language='"${download_installation_language}"'&startTime='"${download_installation_start_time}"'&endTime='"${download_installation_end_time}"'&exportType='"${download_installation_export_type}" \
   -H 'Authorization: Bearer '"${ACCESS_TOKEN}"'' \
@@ -48,7 +65,7 @@ function getInAppPaymentReport()
   curl --silent -X GET \
   'https://connect-api.cloud.huawei.com/api/report/distribution-operation-quality/v1/IAPExport/'"${huawei_app_id}"'?language='"${in_app_payment_language}"'&startTime='"${in_app_payment_start_time}"'&endTime='"${in_app_payment_end_time}"'&exportType='"${in_app_payment_export_type}"'&currency='"${in_app_payment_currency}" \
   -H 'Authorization: Bearer '"${ACCESS_TOKEN}"'' \
-  -H 'client_id: '"${huawei_client_id}"'' > DownloadAndInstallationReport.json
+  -H 'client_id: '"${huawei_client_id}"'' > InAppPaymentReport.json
   REPORT_LINK=`jq -r '.fileURL' InAppPaymentReport.json` 
   envman add --key "IN_APP_PAYMENT_REPORT_REPORT_LINK" --value "$REPORT_LINK"
   printf "Log URL obtained (\${IN_APP_PAYMENT_REPORT_REPORT_LINK}=${REPORT_LINK})"
@@ -59,7 +76,6 @@ function getInAppPaymentReport()
 function getPaidDownloadReport()
 {
   ACCESS_TOKEN=`jq -r '.access_token' token.json` 
-  # Get a token depending on the domain selected
   DOMAIN=$( getDomainFromSelection ) 
 
   printf "\nGetting Paid Download Report\n"
@@ -84,7 +100,7 @@ function getInstallationFailureDataReport()
   curl --silent -X GET \
   'https://connect-api.cloud.huawei.com/api/report/distribution-operation-quality/v1/appDownloadFailExport/'"${huawei_app_id}"'?language='"${installation_failure_data_language}"'&startTime='"${installation_failure_data_start_time}"'&endTime='"${installation_failure_data_end_time}" \
   -H 'Authorization: Bearer '"${ACCESS_TOKEN}"'' \
-  -H 'client_id: '"${huawei_client_id}"'' > PaidDownloadReport.json
+  -H 'client_id: '"${huawei_client_id}"'' > InstallationFailureDataReport.json
   REPORT_LINK=`jq -r '.fileURL' InstallationFailureDataReport.json` 
   envman add --key "INSTALLATION_FAILURE_DATA_REPORT_LINK" --value "$REPORT_LINK"
   printf "Log URL obtained (\${INSTALLATION_FAILURE_DATA_REPORT_LINK}=${REPORT_LINK})"
@@ -108,12 +124,12 @@ function getDomainFromSelection()
 
 getToken  
 
-getDownloadAndInstallationReport 
+#getDownloadAndInstallationReport 
 
 getInAppPaymentReport
 
-getPaidDownloadReport
+#getPaidDownloadReport
 
-getInstallationFailureDataReport
+#getInstallationFailureDataReport
 
 exit 0
